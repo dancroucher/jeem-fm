32d48aa0a2ff3553fb978c00161dcb7fd481744e
dancroucher <dan.croucher@gmail.com>
2025-12-08 20:33:44 +0000

    Update player.js

--- src/player.js
+++ src/player.js
@@ -98,7 +98,7 @@ window.onload = function() {
 document.addEventListener('DOMContentLoaded', function() {
         setTimeout(function(){
     //mp4background.play();
-    start.innerHTML = "<span>Enter Playlist URL or ID: <br><br><form><input class='videobox' type=\"text\" id=\"video-entry\" name=\"video-entry\"><input type=\"submit\" value=\"Submit\" onclick='submitVideoName()'></form></span>";
+    start.innerHTML = "<span>Enter Playlist URL or ID: <br><br><input class='videobox' type=\"text\" id=\"video-entry\" name=\"video-entry\">&nbsp;&nbsp;<input type=\"submit\" value=\"Submit\" onclick='submitVideoName()'></span>";
     //backgroundAuto.style.display="none";
 
     }, 0);
